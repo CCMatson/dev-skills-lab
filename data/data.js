8232d5eddde8dbade3a9b7e9ea9a4832de3ddb34
CCMatson <catherine.matson@gmail.com>
2023-01-05 18:28:31 -0800

    initial commit

---
+++ data/data.js
@@ -0,0 +1,7 @@
+const devSkills = [
+  {skill: 'Javascript', done: true, _id: 1 },
+  {skill: 'HTML', done: true , _id: 2},
+  {skill: 'CSS', done: true, _id: 3},
+  {skill: 'MongoDB', done: false, _id: 4},
+  {skill: 'NodeExpress', done: false, _id}
+]
