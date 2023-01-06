0b7a036ef3ffe477e25abc0fb201863801849e71
CCMatson <catherine.matson@gmail.com>
2023-01-06 09:15:20 -0800

    add a controller

---
+++ controllers/skills.js
@@ -0,0 +1,18 @@
+import { Skill } from '../models/skill.js'
+
+function index(req, res) {
+  Skill.find({})
+  .then(skills => {
+    res.render('skills/index', {
+      skills: skills
+    })
+  })
+  .catch(error => { // If there's an error, console.log it and redirect back home!
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+export {
+  index
+}
--- data/dev-data.js
+++ data/dev-data.js
@@ -1,7 +1,12 @@
-const devSkills = [
+const skills = [
   {skill: 'Javascript', done: true, _id: 1 },
   {skill: 'HTML', done: true , _id: 2},
   {skill: 'CSS', done: true, _id: 3},
   {skill: 'MongoDB', done: false, _id: 4},
-  {skill: 'NodeExpress', done: false, _id}
+  {skill: 'Express', done: false, _id: 5},
+  {skill: 'Node', done: false, _id: 6}
 ]
+
+export {
+  skills
+}
