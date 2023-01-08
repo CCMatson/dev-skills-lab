4f7e22a6464f0339817d22cb1f81d4c93fc8cf97
CCMatson <catherine.matson@gmail.com>
2023-01-07 19:48:08 -0800

    add delete buttons

--- controllers/skills.js
+++ controllers/skills.js
@@ -4,7 +4,8 @@ function index(req, res) {
   Skill.find({})
   .then(skills => {
     res.render('skills/index', {
-      skills: skills
+      skills: skills,
+      time: req.time
     })
   })
   .catch(error => { // If there's an error, console.log it and redirect back home!
@@ -42,9 +43,23 @@ function show(req, res){
   })
 }
 
+function deleteSkills(req, res){
+  Skill.findByIdAndDelete(req.params.id)
+  .then(skill => {
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+})
+}
+
+
+
 export {
   index,
   newSkill as new,
   create,
-  show
+  show,
+  deleteSkills as delete
 }
